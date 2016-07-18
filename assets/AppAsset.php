9e81d1968eee8898a292a552e275142fcd2c85cf
Maxi <maxibriosso@gmail.com>
2016-07-18 15:57:07 -0300

    Se borran widgets.

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -22,24 +22,31 @@ class AppAsset extends AssetBundle
         'css/animate.css',
         'css/font-awesome.css',
         'css/nifty.min.css',
-        'css/bootstrap-fileinput/css/fileinput.min.css',
+        'css/kartik-v-bootstrap-fileinput/css/fileinput.min.css',
         'lineicons/style.css',
         'css/jquery.steps.css',
         'css/estilo.css',
         'css/responsive.css',
     ];
     public $js = [
+        //'js/jquery.js',
         'js/jquery.nicescroll.js',
         'js/jquery.dcjqaccordion.js',
         'js/jquery.steps.min.js',
         'js/wow.min.js',
         'js/bootstrap-select.js',
-        'js/bootstrap-fileinput/js/plugins/canvas-to-blob.min.js',
-        'js/bootstrap-fileinput/js/fileinput.min.js',
-        'js/bootstrap-fileinput/js/fileinput_locale_es.js',
+        /*'js/kartik-v-bootstrap-fileinput/js/plugins/canvas-to-blob.min.js',
+        'js/kartik-v-bootstrap-fileinput/js/plugins/sortable.min.js',
+        'js/kartik-v-bootstrap-fileinput/js/plugins/purify.min.js',
+        'js/kartik-v-bootstrap-fileinput/js/fileinput.min.js',
+        'js/bootstrap.js',
+        'js/kartik-v-bootstrap-fileinput/themes/fa/theme.js',
+        'js/kartik-v-bootstrap-fileinput/js/locales/es.js',
+        //boostrap.js
+        //'js/kartik-v-bootstrap-fileinput/js/fileinput_locale_es.js',*/
         'js/admin.js',
-        /*'js/altaImagenes.js',
-        'js/modificarImagenes.js',*/
+        'js/altaImagenes.js',
+        'js/modificarImagenes.js',
         'js/modificarRemateImagenes.js',       
     ];
     public $depends = [
